74f43a885aa1f42ee850b791461f6d2fb7760d4a
Isabel Paredes <isabel.paredes@quantstack.net>
2023-11-07 17:07:57 +0100

    Publish cmd_vel messages for turtle

--- blocks/HARDWARE/ROBOTICS/ROS2/ROS_PUBLISHER/ROS_PUBLISHER.py
+++ blocks/HARDWARE/ROBOTICS/ROS2/ROS_PUBLISHER/ROS_PUBLISHER.py
@@ -2,29 +2,37 @@ from flojoy import flojoy, Scalar, TextBlob
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 
-class TurtleSimPublisher(Node):
+class Publisher(Node):
 
+    # TODO: change topic name and node name to a block param
+    # TODO: support other message types
     def __init__(self):
         super().__init__('flojoy_pub')
         self.publisher_ = self.create_publisher(String, 'flojoy_topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-        self.msg_blob = TextBlob("empty")
+        self.publisher_vel = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
-    def timer_callback(self):
+    def publish_msg(self, msg_str="no message"):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg.data = msg_str
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-        self.msg_blob = TextBlob(msg.data)
+        return TextBlob(msg.data)
 
-        if self.i > 10:
-            self.destroy_node()
-            rclpy.shutdown()
+    def publish_vel(self):
+        msg = Twist()
+        # msg.linear.x = 1.0
+        # msg.linear.y = 2.0
+        msg.angular.z = 6.0
+        self.publisher_vel.publish(msg)
+        return TextBlob(str([msg.linear.x,
+                             msg.linear.y,
+                             msg.linear.z,
+                             msg.angular.x,
+                             msg.angular.y,
+                             msg.angular.z]))
+
 
 @flojoy
 def ROS_PUBLISHER(
@@ -33,15 +41,14 @@ def ROS_PUBLISHER(
     if not rclpy.ok():
         rclpy.init()
 
-    turtle_pub = TurtleSimPublisher()
+    ros_pub = Publisher()
+    ros_pub.publish_msg("tst tst")
+    out = ros_pub.publish_vel()
 
-    rclpy.spin(turtle_pub)
+    rclpy.spin_once(ros_pub, timeout_sec=1)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    # minimal_publisher.destroy_node()
-    # rclpy.shutdown()
+    ros_pub.destroy_node()
+    rclpy.shutdown()
 
-    return turtle_pub.msg_blob or TextBlob( x.c * 3 )
+    return out or TextBlob( x.c * 3 )
 
